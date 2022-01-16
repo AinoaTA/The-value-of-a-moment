3a9eca43b5552479ca20c6acc392cd55e72dbb25
adribasterra <adrianabasterraa@gmail.com>
2022-01-16 18:22:16 +0100

    feat: bitterGame

--- Assets/Scripts/Minigames/BitterMinigame.cs
+++ Assets/Scripts/Minigames/BitterMinigame.cs
@@ -4,15 +4,67 @@ using UnityEngine;
 
 public class BitterMinigame : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject beetParent;
+    private GameObject[] beets;
+    private GameObject activeBeet;
+    private int counter;
+
     void Start()
     {
-        
+        counter = 0;
+        int numBeets = beetParent.transform.childCount;
+        beets = new GameObject[numBeets];
+        for(int i = 0; i < numBeets; ++i)
+        {
+            GameObject beat = beetParent.transform.GetChild(i).gameObject;
+            beat.SetActive(false);
+            beets[i] = beat;
+        }
+
+        activeBeet = beets[counter];
+        activeBeet.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ManageLikeButton()
     {
-        
+        if (activeBeet.name.Contains("troll"))
+        {
+            // Disminuir confianza
+        }
+        else if (activeBeet.name.Contains("amig"))
+        {
+            // Aumentar confianza
+        }
+        else
+        {
+            Debug.Log("Error. User cannot be recognizable");
+        }
+        ActivateNextBeet();
+    }
+
+    public void ManageDiscardButton()
+    {
+        if (activeBeet.name.Contains("troll"))
+        {
+            // Aumentar confianza
+        }
+        else if (activeBeet.name.Contains("amig"))
+        {
+            // Disminuir confianza
+        }
+        else
+        {
+            Debug.Log("Error. User cannot be recognizable");
+        }
+        ActivateNextBeet();
+    }
+
+    private void ActivateNextBeet()
+    {
+        activeBeet.SetActive(false);
+        counter++;
+        activeBeet = beets[counter];
+        activeBeet.SetActive(true);
     }
 }
