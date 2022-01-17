71b8dd5d4085a6695f7439ca32cdfef1739dee5e
AinoaTA <atabares@edu.tecnocampus.cat>
2022-01-17 01:32:46 +0100

    Arreglo canvas minigame

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -11,6 +11,8 @@ public class PlayerController : MonoBehaviour
     public bool yAxisInverted;
     public Transform m_PlayerWakeUp;
     public Transform m_PlayerSleep;
+    private float m_VerticalSpeed = 1;
+    private bool m_OnGround;
     public enum PlayerState
     {
         IDLE = 0,
@@ -38,12 +40,35 @@ public class PlayerController : MonoBehaviour
     {
        
         Vector3 keyInput = new Vector3(Input.GetAxis("Horizontal")*(xAxisInverted ? -1.0f : 1.0f), 0, Input.GetAxis("Vertical") * (yAxisInverted ? -1.0f : 1.0f)); //está la cámara mirando desde un lado
-        controller.Move((transform.TransformDirection(keyInput) * movementSpeed) * Time.deltaTime);
+        Vector3 l_movement = (transform.TransformDirection(keyInput) * movementSpeed) * Time.deltaTime;
+        
+
+
+        l_movement.y = m_VerticalSpeed * Time.deltaTime;
+        m_VerticalSpeed += Physics.gravity.y * Time.deltaTime;
+
+        //controller.Move(l_movement);
+        Gravity(l_movement);
         // Update animation (if so)
         //if (m_PlayerState != PlayerState.IDLE && keyInput == Vector3.zero)
         //    m_PlayerState = PlayerState.IDLE;
         //else if (m_PlayerState != PlayerState.MOVE)
         //    m_PlayerState = PlayerState.MOVE;
+    }
+
+    private void Gravity(Vector3 movement)
+    {
+        CollisionFlags l_CollisionFlags = controller.Move(movement);
+        if ((l_CollisionFlags & CollisionFlags.Below) != 0)
+        {
+            m_OnGround = true;
+            m_VerticalSpeed = 0.0f;
+        }
+        else
+            m_OnGround = false;
+        if ((l_CollisionFlags & CollisionFlags.Above) != 0 && m_VerticalSpeed > 0.0f)
+            m_VerticalSpeed = 0.0f;
+
     }
     private void Update()
     {
