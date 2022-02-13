239d6cac386a12cbca6c0dcf53daac06889afe6d
AinoaTA <atabares@edu.tecnocampus.cat>
2022-02-13 17:51:43 +0100

    subida de respaldo

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,19 +6,14 @@ public class PlayerController : MonoBehaviour
     private CharacterController controller;
     public float movementSpeed = 2f;
     public PlayerAnimations anim;
-    public PlayerState m_PlayerState;
+    //public PlayerState m_PlayerState;
     public bool xAxisInverted;
     public bool yAxisInverted;
     public Transform m_PlayerWakeUp;
     public Transform m_PlayerSleep;
     private float m_VerticalSpeed = 1;
     private bool m_OnGround;
-    public enum PlayerState
-    {
-        //SLEEP=0,
-        //IDLE,
-        //MOVE,
-    }
+
 
     //private bool busy;
 
@@ -43,19 +38,12 @@ public class PlayerController : MonoBehaviour
         Vector3 keyInput = new Vector3(Input.GetAxis("Horizontal")*(xAxisInverted ? -1.0f : 1.0f), 0, Input.GetAxis("Vertical") * (yAxisInverted ? -1.0f : 1.0f)); //está la cámara mirando desde un lado
         Vector3 l_movement = (transform.TransformDirection(keyInput) * movementSpeed) * Time.deltaTime;
         
-
-
         l_movement.y = m_VerticalSpeed * Time.deltaTime;
         m_VerticalSpeed += Physics.gravity.y * Time.deltaTime;
 
-        //controller.Move(l_movement);
+        
         Gravity(l_movement);
-        //Update animation(if so)
-
-            //if (m_PlayerState != PlayerState.IDLE && keyInput == Vector3.zero)
-            //    m_PlayerState = PlayerState.IDLE;
-            //else if (m_PlayerState != PlayerState.MOVE)
-            //    m_PlayerState = PlayerState.MOVE;
+        
     }
 
     private void Gravity(Vector3 movement)
@@ -74,29 +62,13 @@ public class PlayerController : MonoBehaviour
     }
     private void Update()
     {
-        if (GameManager.GetManager().m_CurrentStateGame== GameManager.StateGame.GamePlay)
-        {
-            Move();
-            //SetAnimations();
-        }
+        //if (GameManager.GetManager().m_CurrentStateGame== GameManager.StateGame.GamePlay)
+        //{
+        //    Move();
+        //    //SetAnimations();
+        //}
     }
-    //public void SetIdle()
-    //{
-    //    m_PlayerState = PlayerState.IDLE;
-    //}
-
-    //private void SetAnimations()
-    //{
-    //    if (controller.velocity.magnitude <= 0.00f)
-    //    {
-    //        anim.SetMovement(0);
-    //    }
-    //    else if (controller.velocity.magnitude >= 0.1f)
-    //    {
-    //        print("a");
-    //        anim.SetMovement(controller.velocity.magnitude);
-    //    }
-    //}
+    
 
     public void PlayerWakeUpPos()
     {
