21f783aea25944736ccd56842c79149660743b04
adribasterra <adrianabasterraa@gmail.com>
2022-06-27 01:01:20 +0200

    michi miau

--- Assets/Scripts/Controllers/MichiController.cs
+++ Assets/Scripts/Controllers/MichiController.cs
@@ -10,6 +10,8 @@ public class MichiController : MonoBehaviour
     private int i;
     public Vector3 newPos;
     private float turningRate = 3f;
+    private Quaternion targetRotation;
+    private bool reset;
 
     [Range(0.1f, 2f)] public float walkSpeed;
 
@@ -18,38 +20,36 @@ public class MichiController : MonoBehaviour
     {
         animator = this.GetComponent<Animator>();
         i = 0;
-        path = null;
+        reset = true;
+        animator.SetBool("walking", true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (path == null)   // Create new path
+        if (reset)
         {
-            if(i != 0 && !animator.GetAnimatorTransitionInfo(0).IsName("miau -> idle")) return;
+            // if(!animator.GetAnimatorTransitionInfo(0).IsName("idle -> walk")) return;
             // Calculate new random position
             float xDist = Random.Range(-10.0f, 10.0f);
             float zDist = Random.Range(-10.0f, 10.0f);
-
             newPos = new Vector3(xDist, this.transform.position.y,zDist);
-            path = "dfa";
+            targetRotation = Quaternion.LookRotation(newPos - this.transform.position);
+            reset = false;
             // Debug.Log("michi newpos: " + newPos);
-
             animator.SetBool("walking", true);
         }
         else
         {
+            if(animator.GetAnimatorTransitionInfo(0).IsName("miau -> idle")) reset = true;
+            if(animator.GetCurrentAnimatorStateInfo(0).IsName("sitting")) return;
             this.transform.position = Vector3.MoveTowards(this.transform.position, newPos, walkSpeed * Time.deltaTime);
-            // this.transform.LookAt(newPos);
-            var targetRotation = Quaternion.LookRotation(newPos - this.transform.position);
             this.transform.localRotation = Quaternion.Slerp(this.transform.rotation, targetRotation, turningRate * Time.deltaTime);
 
             if (Vector3.Distance(transform.position, newPos) < 2f)
             {
                 Debug.Log("MICHI has arrived");
-                path = null;
-                animator.SetBool("walking", false);
-                animator.SetTrigger("hasArrived");
+                Miau();
             }
         }
     }
@@ -58,10 +58,15 @@ public class MichiController : MonoBehaviour
     {
         if(other.gameObject.layer != 9)
         {
-            animator.SetBool("walking", false);
-            animator.SetTrigger("hasArrived");
-            Debug.Log("Michi collided with " + other.gameObject);
-            path = null;
+            Miau();
         }
+    }
+
+    private void Miau()
+    {
+        Debug.Log("Miau");
+        animator.SetBool("walking", false);
+        animator.ResetTrigger("hasArrived");
+        animator.SetTrigger("hasArrived");
     }
 }
