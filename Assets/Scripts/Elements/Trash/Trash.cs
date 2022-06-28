2d722c5e6e8ea44245e60c64625b9f810d571703
adribasterra <adrianabasterraa@gmail.com>
2022-06-28 15:18:39 +0200

    grabbing

--- Assets/Scripts/Elements/Trash/Trash.cs
+++ Assets/Scripts/Elements/Trash/Trash.cs
@@ -5,29 +5,44 @@ public class Trash : Interactables
 {
     public enum TrashType { CLOTHES, TRASH }
     public TrashType type = TrashType.TRASH;
+    // 5 prendas de ropa
+    public int maxTras = 5;
+    public Transform target;
 
-    //5 prendas de ropas
-
-
+    private int numberTrash;
+    private float grabbingSpeed = 2f;
+    private bool grabbing = false;
+    
     private void Start()
     {
         GameManager.GetManager().trashes.Add(this);
     }
-    private int numberTrash;
-    public int maxTras=5;
+
     public override void Interaction(int optionNumber)
     {
         switch (optionNumber)
         {
             case 1:
+                grabbing = true;
+                break;
+        }
+    }
 
+    private void Update()
+    {
+        if(grabbing)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, target.position, grabbingSpeed * Time.deltaTime);
+
+            if(Vector3.Distance(this.transform.position, target.position) < 0.1f)
+            {
                 if (type == TrashType.TRASH)
                     GameManager.GetManager().InventoryTrash.AddTrash(this);
                 else
                     GameManager.GetManager().InventoryTrash.AddDirtyClothes(this);
 
-                gameObject.SetActive(false);
-                break;
+                this.gameObject.SetActive(false);
+            }
         }
     }
 
