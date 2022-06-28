cf4001615147b8eb4623c1d956522535fb2588fa
adribasterra <adrianabasterraa@gmail.com>
2022-06-28 23:31:49 +0200

    Revert "code"
    This reverts commit 162829515588e392e9a8af567d7d4ce9ce64f482.

--- Assets/Scripts/Elements/Bed.cs
+++ Assets/Scripts/Elements/Bed.cs
@@ -76,6 +76,7 @@ public class Bed : Interactables
     {
         if (Input.GetKeyDown(KeyCode.Escape) && gameInitialized)
         {
+            Debug.Log("Exit");
             Exit();
         }
     }
@@ -110,7 +111,7 @@ public class Bed : Interactables
 
     private void OnMouseUp()
     {
-        if (m_Done&& gameInitialized)
+        if (m_Done && gameInitialized)
             BedDone();
     }
 
@@ -134,7 +135,6 @@ public class Bed : Interactables
         sleepTextBed.transform.localPosition = lastPosDormirText;
         GameManager.GetManager().dayNightCycle.TaskDone();
         GameManager.GetManager().StartThirdPersonCamera();
-        print("bed");
         GameManager.GetManager().Autocontrol.AddAutoControl(m_MinAutoControl);
     }
 
@@ -201,6 +201,7 @@ public class Bed : Interactables
         GameManager.GetManager().ProgramMinigame.ResetAllGame();
         GameManager.GetManager().bucket.ResetInteractable();
         GameManager.GetManager().Mirror.ResetInteractable();
+        GameManager.GetManager().ResetTrash();
         //GameManager.GetManager().Book.ResetInteractable();
         //GameManager.GetManager().VR.ResetVRDay();
 
