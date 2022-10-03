44282a33b518160fc7bc070ce4084d5d745c97e7
AinoaTA <atabares@edu.tecnocampus.cat>
2022-10-03 21:11:59 +0200

    drum

---
+++ Assets/Scripts/Interactables/Drum.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Drum : Interactables
+{
+    public override void Interaction(int optionNumber)
+    {
+        base.Interaction(optionNumber);
+        switch (optionNumber)
+        {
+            case 1:
+                GameManager.GetManager().gameStateController.ChangeGameState(2);
+                // Inicia minijuego
+                GameManager.GetManager().cameraController.StartInteractCam(nameInteractable);
+                break;
+        }
+    }
+
+    public override void ExitInteraction()
+    {
+        GameManager.GetManager().StartThirdPersonCamera();
+        base.ExitInteraction();
+    }
+}
