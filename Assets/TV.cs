9a5e7f51875001b1f921ffefe66fdfd429a007ce
Adriana <adrianabasterraa@gmail.com>
2022-10-04 21:01:27 +0200

    tv channels

--- Assets/TV.cs
+++ Assets/TV.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class TV : Interactables
 {
+    public GameObject screen;
+    // handle de canales
+    private int[] channels;
+    private int currChannel;    // Esto permite guardar el canal al apagar la tele, como irl
+
+    private void Start()
+    {
+        screen.SetActive(false);
+        currChannel = 0;
+    }
+
     public override void Interaction(int optionNumber)
     {
         base.Interaction(optionNumber);
@@ -12,13 +23,22 @@ public class TV : Interactables
             case 1:
                 GameManager.GetManager().gameStateController.ChangeGameState(2);
                 GameManager.GetManager().cameraController.StartInteractCam(nameInteractable);
+                screen.SetActive(true);
                 break;
         }
     }
 
     public override void ExitInteraction()
     {
+        screen.SetActive(false);
         GameManager.GetManager().StartThirdPersonCamera();
         base.ExitInteraction();
+    }
+
+    public void ChangeChannel()
+    {
+        // 1 función para subir o bajar canal o 2 funciones
+        currChannel++;
+        // Set screen to channels[currChannel]
     }
 }
