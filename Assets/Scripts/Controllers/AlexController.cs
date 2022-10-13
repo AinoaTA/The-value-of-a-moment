d83781d1ad1afd6666c68664dc48be936fa8d89e
Adriana <adrianabasterraa@gmail.com>
2022-10-13 18:37:33 +0200

    alex is gone

--- Assets/Scripts/Controllers/AlexController.cs
+++ Assets/Scripts/Controllers/AlexController.cs
@@ -9,14 +9,24 @@ public class AlexController : MonoBehaviour
     private NavMeshAgent navMeshAgent;
     public Transform exitTransform;
 
+    private bool isGone;
+
     void Start()
     {
+        isGone = false;
         navMeshAgent = this.GetComponent<NavMeshAgent>();
         ATuCasa();
     }
 
     void Update()
     {
+        if (isGone) return;
+
+        if (Vector3.Distance(transform.position, exitTransform.position) < .2f)
+        {
+            this.gameObject.SetActive(false);
+            isGone = true;
+        }
     }
 
     private void ATuCasa()
