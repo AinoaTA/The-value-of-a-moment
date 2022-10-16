d8947a13e76c564772f3f644cb0fe53ef11665ca
Adriana <adrianabasterraa@gmail.com>
2022-10-16 23:32:51 +0200

    Update FinalSceneBar.cs

--- Assets/Scripts/Other/FinalSceneBar.cs
+++ Assets/Scripts/Other/FinalSceneBar.cs
@@ -5,16 +5,36 @@ using UnityEngine;
 public class FinalSceneBar : MonoBehaviour
 {
     public Transform barPivot;
-    private float autoconfidence;
+    public float scaleSpeed;
+
+    private float autoconfidence, maxAutoConfidence;
+    private bool activated;
+    private Vector3 newScale;
 
     void Start()
     {
-        autoconfidence = GameManager.GetManager().autocontrol.GetAutcontrolValue();
-        Debug.Log(autoconfidence);
+        autoconfidence = 20f;
+        activated = false;
+        maxAutoConfidence = 100f;
+        //autoconfidence = GameManager.GetManager().autocontrol.GetAutcontrolValue();
+        //Debug.Log(autoconfidence);
+        float autoconfidenceFactor = autoconfidence / maxAutoConfidence;
+        newScale = new Vector3(autoconfidenceFactor, 1f, 1f);
+        ActivateAnim();
     }
 
     void Update()
     {
-        
+        if (activated)
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, newScale, scaleSpeed * Time.deltaTime);
+            //if (transform.localScale.magnitude == newScale.magnitude)
+            //    activated = false;
+        }
+    }
+
+    public void ActivateAnim()
+    {
+        activated = true;
     }
 }
