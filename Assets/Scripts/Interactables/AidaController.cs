49adc947ecff318c9c1e5cd818487b23e3db2630
AinoaTA <atabares@edu.tecnocampus.cat>
2022-10-16 14:06:35 +0200

    ew

---
+++ Assets/Scripts/Interactables/AidaController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AidaController : MonoBehaviour
+{
+    public NavMeshAgent navMesh;
+
+    private void Start()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+    }
+
+
+
+
+
+
+
+}
