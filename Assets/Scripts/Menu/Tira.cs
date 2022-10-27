71664e5cc0d78cc670918ae9101ba8efbc3bea8d
AinoaTA <atabares@edu.tecnocampus.cat>
2022-10-27 17:07:05 +0200

    intro tutorial

--- Assets/Scripts/Menu/Tira.cs
+++ Assets/Scripts/Menu/Tira.cs
@@ -8,19 +8,27 @@ public class Tira : MonoBehaviour
     [SerializeField] bool played;
     //[SerializeField] string nameAudio;
     [SerializeField] bool finalRecord;
-
+    public GameObject tutorial;
     public string soundEvent = null;
 
     bool block;
     public delegate void EndButton();
     public static EndButton buttonEnd;
 
+    private IEnumerator Start()
+    {
+        if (tutorial == null) yield break;
+        yield return new WaitForSeconds(2.5f);
+        tutorial.gameObject.SetActive(true);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("ComicAudio"))
         {
+            if (tutorial != null)
+                tutorial.gameObject.SetActive(false);
             GameManager.GetManager().dialogueManager.SetDialogue(soundEvent, canRepeat: true, onlyVoice:true);
-            
                 if (finalRecord && !block)
                 {
                     block = true;
@@ -28,6 +36,7 @@ public class Tira : MonoBehaviour
                 }
         }
     }
+
 
     IEnumerator FinalRecord()
     {
